aea9a9f7d7195f32caa4fa29189cb5d5c4b5e425
owen550 <omoses611@gmail.com>
2026-07-07 15:06:49 +0700

    fe ilang njir

--- fe/app/src/main/java/com/example/m10/data/sources/remote/RemoteDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.m10.data.sources.remote
-
-import com.example.m10.data.Todo
-
-interface RemoteDataSource {
-    suspend fun put(todo: Todo) : Todo
-    suspend fun sync(todos: List<Todo>) : List<Todo>
-}
