60dc500fa1aeb1afc488ea2f669f989dcdd1aac8
owen550 <omoses611@gmail.com>
2026-07-07 23:12:28 +0700

    profile dan navigation udah selesai ui nya

---
+++ fe/app/src/main/java/com/example/fe/ui/todo/GradeSummary.kt
@@ -0,0 +1,24 @@
+package com.example.fe.ui.todo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fe.R
+
+
+class GradeSummary : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_grade_summary, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
