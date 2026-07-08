6b62aec1ad9084d5ea3496c068538ffbfbf2d4de
owen550 <omoses611@gmail.com>
2026-07-08 13:55:57 +0700

    garade sumarry fix

--- fe/app/src/main/java/com/example/fe/ui/todo/GradeSummary.kt
+++ fe/app/src/main/java/com/example/fe/ui/todo/GradeSummary.kt
@@ -6,19 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.fe.R
+import com.example.fe.databinding.FragmentDashboardBinding
+import com.example.fe.databinding.FragmentGradeSummaryBinding
 
 
 class GradeSummary : Fragment() {
-
+    lateinit var binding: FragmentGradeSummaryBinding // ini tolong disesiuaikan di masing masing !!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grade_summary, container, false)
+        binding = FragmentGradeSummaryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // main code
     }
 }
